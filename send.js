c21ba2592fd98231d209bd2caee7113067def0aa
jsbase <andre.siedersleben@gmail.com>
2022-03-23 03:56:09 +0100

    Chane: examples

--- send.js
+++ send.js
@@ -2,6 +2,6 @@
 // ~> Just setting Content-Length
 // ~> because Morgan wants to print it
 exports.send = function (res, data) {
-	res.setHeader('Content-Length', data.length);
-	res.end(data);
-}
+    res.setHeader('Content-Length', data.length);
+    res.end(data);
+};
--- simple-server.js
+++ simple-server.js
@@ -14,9 +14,9 @@ TorrentSearchApi.enablePublicProviders();
 server()
     .use(cors())
     .use(morgan('combined'))
-	.use(sirv('public'))
-	.use(parser.json())
-	.post('/magnets', async (req, res) => {
+    .use(sirv('public'))
+    .use(parser.json())
+    .post('/magnets', async (req, res) => {
         try {
             // console.log(' \n query: ', JSON.stringify(req.query), ' \n ');
 
@@ -30,16 +30,19 @@ server()
             console.log(` category:  ${category}`);
             console.log(` limit:  ${limit} \n `);
 
-            const torrents = await TorrentSearchApi.search(query, category, limit);
+            const torrents = await TorrentSearchApi.search(
+                query,
+                category,
+                limit
+            );
             console.log(' \n torrents: ', torrents, ' \n ');
 
             // const getMagnet = async (_torrent) => await TorrentSearchApi.getMagnet(_torrent);
             // const magnets = Array.from(torrents).map(torrent => getMagnet(torrent));
             // console.log(' \n magnets: ', magnets, ' \n ');
 
-
             send(res, 200, torrents);
-        } catch(err) {
+        } catch (err) {
             send(res, 404);
         }
 
@@ -49,7 +52,7 @@ server()
 
         let json = JSON.stringify(req.query);
 		res.end(json); */
-	})
-	.listen(PORT, () => {
-		console.log(`> Running on localhost:${PORT}`);
-	});
+    })
+    .listen(PORT, () => {
+        console.log(`> Running on localhost:${PORT}`);
+    });

e620be444212d74850906afb20a4513a0988206d
jsbase <andre.siedersleben@gmail.com>
2022-03-29 22:38:26 +0200

    Change: server

--- client/src/components/SearchForm.vue
+++ client/src/components/SearchForm.vue
@@ -1,13 +1,13 @@
 <template>
-  <main>
-    <section id="poster"></section>
-    <section id="layout">
+  <section>
+    <div id="poster"></div>
+    <div id="layout">
       <h1>magnet search</h1>
       <form class="pure-form pure-form-stacked" v-on:submit.prevent="submit">
         <fieldset class="pure-control-group">
           <label for="query">Search</label>
           <input
-            v-model.trim="formValue.query"
+            v-model="formValue.query"
             id="query"
             class="form-control"
             type="text"
@@ -19,7 +19,7 @@
         <fieldset class="pure-control-group">
           <label for="category">Category</label>
           <input
-            v-model.trim="formValue.category"
+            v-model="formValue.category"
             id="category"
             class="form-control"
             type="text"
@@ -29,7 +29,7 @@
 
           <label for="limit">Limit</label>
           <input
-            v-model.number="formValue.limit"
+            v-model="formValue.limit"
             id="limit"
             class="form-control"
             type="number"
@@ -42,15 +42,15 @@
         </button>
       </form>
       <ul>
-        <li v-for="(torrent, idx) in torrents" :key="idx">
-          <h4 v-if="torrent.title" class="mt-0 mb-1">
-            <strong>{{ idx + 1 }}.</strong> {{ torrent.title }}
+        <li v-for="torrent in torrents" :key="torrent.title">
+          <h4 class="mt-0 mb-1">
+            {{ torrent.title }}
           </h4>
-          <p v-if="torrent.seeds" class="mt-0 mb-1">
+          <p class="mt-0 mb-1">
             <span>Seeds:</span>
             {{ torrent.seeds }}
           </p>
-          <p v-if="torrent.peers" class="mt-0 mb-1">
+          <p class="mt-0 mb-1">
             <span>Peers:</span>
             {{ torrent.peers }}
           </p>
@@ -60,82 +60,144 @@
             <a :href="torrent.desc">{{ torrent.desc }}</a>
           </p>
         </li>
+        <!--
+                                                        <li v-if="torrents && torrents.length === 0">No results.</li>
+                                                        <li v-if="error">{{ error }}</li>
+                                                        -->
       </ul>
-    </section>
-  </main>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "SearchForm",
 
-  computed: {
+  /* computed: {
     log: function () {
       return console.table(this.torrents);
     }
+    //url: `${this.protocol}://${this.host}:${this.port}${this.torrentsApi}
   },
+  */
 
-  data: function () {
+  data() {
     return {
+      url: "http://localhost:3000/torrents",
       formValue: {
-        query: "",
-        category: "All",
-        limit: 1
+        query: "1080p",
+        category: "Movies",
+        limit: 3
       },
-      torrents: []
-    };
-  },
-
-  created: function () {
-    const opts = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-      },
-      body: JSON.stringify(this.formValue)
-    };
-    const torrents = this.load("http://localhost:3000/torrents", opts);
-
-    console.log("created: ", torrents);
-
-    torrents.then((torr) => {
-      this.torrents.length = 0;
-      this.torrents = this.torrents.concat(torr);
-    });
-  },
-
-  methods: {
-    load: async (url, opts) => {
-      const response = await fetch(url, opts);
-      const result = await response.json();
-      return result.then((data) => {
-        Promise.resolve(data);
-      });
-    },
-
-    submit: function (event) {
-      event.preventDefault();
-
-      console.log("this.formValue: ", this.formValue);
-
-      const opts = {
+      torrents: [],
+      options: {
         method: "POST",
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
-        },
+          "Content-Type": "application/json; charset=UTF-8",
+          Origin: "http://localhost:8080",
+          "Access-Control-Allow-Origin": "*"
+        }
+      }
+    };
+
+    //error: null,
+    //Referer: "http://localhost:3000/torrents",
+    //Connection: "Keep-Alive",
+    //"Keep-Alive": "timeout=12, max=1000",
+    //"Cache-Control": "no-cache"
+  },
+
+  watch: {
+    submit() {
+      this.torrents = this.torrents.map((t) => t);
+    }
+  },
+
+  /*
+    created() {
+    const options = Object.assign(
+      {
         body: JSON.stringify(this.formValue)
-      };
+      },
+      this.options
+    );
 
-      //console.log(`\n submit \n opts: ${JSON.stringify(opts)} \n`);
+    const result = this.load(this.url, options);
+    //alert(`url: ${this.url} \n options: ${JSON.stringify(options)}`);
 
-      const result = this.load("http://localhost:3000/torrents", opts);
-      result.then((torrents) => {
-        console.log(`\n torrents: ${JSON.stringify(torrents)} \n`);
-        this.torrents.length = 0;
+    result
+      .then(function (torrents) {
+        // console.log("torr: ", torr);
+        //alert(`torrents: ${JSON.stringify(torrents)}`);
         this.torrents = this.torrents.concat(torrents);
+      })
+      .catch(function (error) {
+        this.error = error;
+        //alert(`error: ${typeof error === "string" ? error : JSON.stringify(error)}`);
+        console.error("Error: ", error);
       });
+    },
+    */
+  methods: {
+    async load(url, opts) {
+      //alert(`[load] url: ${url}`);
+      let response = await fetch(url, opts);
+      let data = await response.json();
+      const json = JSON.stringify(data);
+      //alert(`[load] json: ${json}`);
+      const result = JSON.parse(json);
+      return result;
+    },
 
-      return false;
+    submit() {
+      //e.preventDefault();
+      /* alert(
+          `submit \n url: ${this.url} \n formValue: ${JSON.stringify(
+            this.formValue
+          )}`
+        ); */
+      // console.log(`\n formValue: ${JSON.stringify(this.formValue)}`);
+
+      const options = Object.assign(
+        {
+          body: JSON.stringify(this.formValue)
+        },
+        this.options
+      );
+
+      // this.torrents.length = 0;
+      //this.error = null;
+
+      // console.log(`opts: ${JSON.stringify(opts)}`);
+      // alert(`opts: ${JSON.stringify(opts)}`);
+      const result = this.load(this.url, options);
+      //this.torrents = [...result];
+
+      //alert(`this.torrents: ${JSON.stringify(this.torrents)}`);
+      result
+        .then(function (torrents) {
+          alert(`torrents [${typeof torrents}]: ${JSON.stringify(torrents)}`);
+          //this.torrents = this.torrents.concat(torrents);
+
+          alert(
+            `this.torrents [${typeof this.torrents}]: ${JSON.stringify(
+              this.torrents
+            )}`
+          );
+
+          this.torrents = [...torrents];
+
+          //Promise.resolve(this.torrents);
+
+          //return false;
+        })
+        .catch(function (error) {
+          this.error = error.toString();
+          alert(`Error: ${this.error}`);
+          console.error("Error: ", this.error);
+        });
+
+      //return false;
     }
   }
 };
@@ -151,8 +213,12 @@ h1 {
 ul {
   list-style-type: none;
   list-style-position: outside;
-  margin: 1rem auto;
-  width: 80%;
+  margin: 2rem 1rem;
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: stretch;
+  justify-content: stretch;
 }
 li {
   margin-top: 1rem;
@@ -160,12 +226,12 @@ li {
 }
 .pure-button-primary {
   background-color: tomato;
-  border: 1px solid red;
+  border: 1px solid tomato;
   border-radius: 5px;
   color: #fff;
   font-weight: 700;
-  padding: 8px 12px;
-  margin: 15px auto 0;
+  padding: 0.3rem;
+  margin: 1rem 0;
   text-align: center;
   width: 100%;
 }
--- server.js
+++ server.js
@@ -1,91 +1,104 @@
 require("dotenv").config();
-//const {
-//    createServer
-//} = require("https");
-//const {
-//    readFileSync
-//} = require("fs");
+// const {createServer} = require("https");
+// const {readFileSync} = require("fs");
 const polka = require("polka");
 const send = require("@polka/send-type");
-const {
-    json
-} = require("body-parser");
-//const sirv = require("sirv");
-//const compress = require("compression");
-//const morgan = require("morgan");
-//const cookieParser = require("cookie-parser");
+const {json} = require("body-parser");
+const morgan = require("morgan");
+// const compress = require("compression");
+const sirv = require("sirv");
+// const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const headers = require("./headers");
 const statusMsg = require("./status.js");
 const warning = require("./warning");
-//const torrents = require('./torrents');
-//const magnet = require("./magnet");
-//const health = require("./health");
+// const respSize = require("./respSize");
+// const health = require("./health");
+// const torrents = require('./torrents');
+// const magnet = require("./magnet");
 /**
-* @see https://github.com/JimmyLaurent/torrent-search-api
-*/
+ * @see https://github.com/JimmyLaurent/torrent-search-api
+ */
 const TorrentSearchApi = require("torrent-search-api");
 TorrentSearchApi.enablePublicProviders();
 //TorrentSearchApi.disableProvider("1337x");
 
 const {
-    NODE_ENV,
-    HOST,
-    PORT = 3000,
-    TORRENTS_API /* MAGNET_API */
+  NODE_ENV,
+  HOST,
+  PORT = 3000,
+  TORRENTS_API
+  // MAGNET_API
 } = process.env;
 
 const isDev = NODE_ENV !== "production";
 
-//const polkaMiddleware =
-//.use(
-//morgan(isDev ? "dev" : "tiny", {immediate: isDev}),
+// const { handler } =
 //cookieParser(),
-//compress(),
-//sirv("public"),
-// health,
-//)
 polka()
-.use(
-    cors(/*{origin: true}*/),
-    json()
-)
-.post(TORRENTS_API, async (req, res) => {
+  .use(
+    morgan(isDev ? "dev" : "tiny", {
+      immediate: isDev
+    }),
+    cors({
+      origin: "*",
+      methods: ["GET", "POST"],
+      maxAge: 8600
+    }),
+    json(),
+    //compress(),
+    sirv("public")
+  )
+  .post(TORRENTS_API, async (req, res) => {
     try {
-        let json = JSON.stringify(req.body);
-        let {
-            query = "1080p", category = "All", limit = 1
-        } = JSON.parse(json);
-        // console.log(`{ "query": "${query}", "category": "${category}", "limit": ${limit}`);
-        let torrents = await TorrentSearchApi.search(query, category, limit);
-        console.log(`torrents: ${JSON.stringify(torrents)}`);
-        // const filtered = torrents.filter((x) => parseInt(x?.peers) >= 1 && parseInt(x?.seeds) >= 1);
-        if (!torrents || !torrents.length) {
-            send(res, statusMsg.empty, warning.notorrents);
-        } else {
-            send(res, statusMsg.success, torrents, headers.json);
-        }
+      //res.headers["Access-Control-Allow-Origin"] = "*";
+      let jsonStr = JSON.stringify(req.body);
+      let {query = "vscode", category = "App", limit = 1} = JSON.parse(jsonStr);
+
+      console.log(
+        `\n{"query": "${query}", "category": "${category}", "limit": ${limit} }\n`
+      );
+
+      const torrents = await TorrentSearchApi.search(query, category, limit);
+
+      console.log(`\n{ "torrents": ${JSON.stringify(torrents)} }\n`);
+
+      if (!torrents || !torrents.length) {
+        send(res, statusMsg.empty, warning.notorrents);
+      } else {
+        let filtered = torrents.filter(
+          (x) =>
+            x &&
+            x.peers &&
+            parseInt(x.peers) >= 1 &&
+            x.seeds &&
+            parseInt(x.seeds) >= 1
+        );
+
+        send(res, statusMsg.success, filtered, headers.json);
+      }
     } catch (err) {
-        send(res, statusMsg.fail, warning.notorrents);
+      send(res, statusMsg.fail, warning.notorrents);
     }
-})
-.listen(PORT, () => {
+  })
+  .listen(PORT, () => {
     if (isDev) {
-        console.log(`> Running on ${HOST}:${PORT}`);
+      console.log(`> Running on ${HOST}:${PORT}`);
     }
-});
+  });
+
 //.post(TORRENTS_API, torrents)
 //.post(MAGNET_API, magnet)
+
 /*
-createServer(
-    {
-        key: readFileSync("./ssl/magnet-search-form.key"),
-        cert: readFileSync("./ssl/magnet-search-form.crt")
-    },
-    polkaMiddleware
-).listen(PORT, () => {
-    if (isDev) {
-        console.log(`> Running on ${HOST}:${PORT}`);
-    }
+const ssl = {
+  key: readFileSync("./ssl/magnet-search-form.key"),
+  cert: readFileSync("./ssl/magnet-search-form.crt")
+};
+
+createServer(ssl, handler).listen(PORT, () => {
+  if (isDev) {
+    console.log(`> Running on ${HOST}:${PORT}`);
+  }
 });
 */
